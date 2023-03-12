37ced27dfcd72f724fc07b2924b78f7e0f8c9586
Landon Fackrell <landonfackrell12@gmail.com>
2023-03-11 17:16:46 -0700

    Setup favorites functionality

--- main.js
+++ main.js
@@ -21,56 +21,51 @@ function fetchTopics(topics) {
 function setTopicsOnPage(topics) {
 
   const topicListElem = document.getElementById("topicList");
-  topicListElem.innerHTML = "" // Remove all current children
+  topicListElem.innerHTML = ""; // Remove all current children
 
   for (const topic of topics) {
     // let newHtml = getTopicHtml(topic);
-    let button = document.createElement("button")
+    let button = document.createElement("button");
     
-    button.textContent = topic
-    button.className = "btn btn-primary"
+    button.textContent = topic;
+    button.className = "btn btn-primary";
     button.addEventListener("click", () => {
       getAndSetGraph(topic)
-    })
+    });
 
-    topicListElem.appendChild(button)
+    topicListElem.appendChild(button);
   }
 }
 
 function getAndSetGraph(topic) {
   getData(topic)
     .catch(() => {
+      document.getElementById("trendDataHeader").innerText = topic + "\'s Trend data";
       document.getElementById("graph").innerHTML = "<p> Oh no! There was an error retrieving your" 
       + "data for the topic: " + topic + " please try another topic.</p>";
-    })
+    });
 }
 
 function getData(topic) {
   return new Promise((resolve, reject) => {
     //Get the data for a specific topic
-    reject("Implement the database lookup later")
+    reject("Implement the database lookup later");
   })
 }
 
 function getSiteVisits() {
   return new Promise((resolve, reject) => {
     //Get the data for How many site visits there ahve been
-    reject("Implement the database lookup later")
+    reject("Implement the database lookup later");
   })
 }
 
-function getNumFavoriteTopics() {
-  return new Promise((resolve, reject) => {
-    //Get the data for a how many favorite topics they have later
-    reject("Implement the database lookup later")
-  })
-}
-
-function setFavorite() {
-  return new Promise((resolve, reject) => {
-    //Set the favorite in the database later
-    reject("Implement the database lookup later")
-  })
+function setFavorite(topic) {
+  favoriteTopics.append(topic);
+  async function pushFavorites() {
+    //Implement pushing favorites to db later
+  }
+  pushFavorites()
 }
 
 function getFavorites() {
@@ -80,19 +75,34 @@ function getFavorites() {
   })
 }
 
+function setupFavorites() {
+  getFavorites()
+    .then(() => {
+      document.getElementById("numFavoriteTopicsVal").innerText = favoriteTopics.length
+    })
+    .catch(() => {
+      document.getElementById("numFavoriteTopicsVal").innerText = "Err"
+    })
+}
+
+/////////////////////////////////////////////////
+
+//User Data
+let favoriteTopics = []
+
+// Backup Data
+
+let backupTopics = [
+  "Instagram",
+  "Cooking",
+  "Sports",
+  "Rock Climbing"
+]
+
 /////////////////////////////////////////////////
 
 // Main Code
 
 getAndSetTopics()
 
-/////////////////////////////////////////////////
-
-// Backup Data
-
-backupTopics = [
-  "Instagram",
-  "Cooking",
-  "Sports",
-  "Rock Climbing"
-]
+setupFavorites()
